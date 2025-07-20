faf43288b8b0264a0c51c157904845008bd297ed
pinkaroo <122147056+pinkaroo@users.noreply.github.com>
2025-07-20 12:06:59 +0800

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,27 +1,28 @@
 task.spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/pinkaroo/Dew/refs/heads/main/Notification.lua'))()('Dew', 'Executed script!', 3)
-    if getgenv().dew then return end
+end)
 
+if not getgenv().dew then
     local dew = {}
     local lp = game.Players.LocalPlayer
-
+    
     dew.teleport = function(pos)
         if lp.Character then
             lp.Character:PivotTo(CFrame.new(pos))
         end
     end
-
+    
     dew.remote_spy = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua'))()
     end
-
+    
     dew.dex = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/infyiff/backup/main/dex.lua'))()
     end
-
+    
     dew.infinite_yield = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
     end
-
+    
     getgenv().dew = dew
-end)
+end
